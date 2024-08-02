4fe3a856b2facc20cfc66b6460bac71de00065d2
Allan <allanfelipedk@gmail.com>
2024-08-01 21:57:33 -0300

    feat: adding the migrations and finalizing the crud

--- src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PagamentosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PagamentosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PagamentosApplication.class, args);
+    }
 
 }
